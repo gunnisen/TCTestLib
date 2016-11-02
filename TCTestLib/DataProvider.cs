fdffc610a081acca6465e36178cfd1365a87e88f
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-02 13:46:10 +0100

    Snapshot commit.

---
+++ TCTestLib/DataProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace TCTestLib
+{
+    public class DataProvider
+    {
+        public static string Greeting()
+        {
+            return $"Hei fadderuttan fra TCTestLib.DataProvider, versjon {Assembly.GetExecutingAssembly().GetName().Version}!";
+        }
+    }
+}
