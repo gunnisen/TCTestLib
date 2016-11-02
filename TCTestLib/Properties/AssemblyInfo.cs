94c531f345be12598b8c30f69deebcae6a3346a8
Gunnar Sylthe <gunnar.sylthe@tv2.no>
2016-11-02 13:50:41 +0100

    Snapshot commit.

--- TCTestLib/Properties/AssemblyInfo.cs
+++ TCTestLib/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TCTestLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for testing TC functionality.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TV 2 AS")]
 [assembly: AssemblyProduct("TCTestLib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
